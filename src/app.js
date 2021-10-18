34649105da84c78734f8d5501fe65311cdf1101b
benholystep <berkan.stm0@gmail.com>
2021-10-19 00:11:45 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,36 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+// dolarDun = "Ankara"
+let dolarBugun = 9.30
+
+dolarDun = "9.20"
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array / dizi
+//camelCasing
+//PascalCasing
+let konutKrediler = ["Konut kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("</ul>")
+for (let i = 0; i < konutKrediler.length; i++) {
+    console.log("<li>" +konutKrediler[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+
+
+//react
+console.log(konutKrediler)
